17a6ef31d52174cadcce0e2f3c3e1053006becb0
veer260 <57021124+veer260@users.noreply.github.com>
2023-01-18 15:57:39 +0530

    initial setup

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <span className='text-xl font-medium' >XTodo</span>
+    </div>
+  )
+}
+
+export default Navbar;
